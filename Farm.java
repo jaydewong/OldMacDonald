d71bc917dace450d136bef10b152a7e357c8c9fb
JaWong29 <JaWong29@s.sfusd.edu>
2018-10-17 08:22:56 -0700

    fixed namedcow and changed member variables of cow to protected

--- Farm.java
+++ Farm.java
@@ -10,6 +10,7 @@ class Farm
       for(int nI = 0; nI < aBunchOfAnimals.length;nI++){
         System.out.println(aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
       }
+      System.out.println("The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );
     }
 }
    //your code here
--- NamedCow.java
+++ NamedCow.java
@@ -1,11 +1,13 @@
 class NamedCow extends Cow {
   
   private String myName;
-  public NamedCow(String name, String type, String sound){ //fix tmrw
+  public NamedCow(String name, String type, String sound){
     myName = name;
+    myType = type;
+    mySound = sound;
   }
   
-  public getName(){
+  public String getName(){
     return myName;
   }
   
